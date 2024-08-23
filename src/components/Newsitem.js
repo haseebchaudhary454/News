ea1fc339c60232aaef5f5f230109d6073a8d62cf
Muhammad Haseeb <haseebtaga453@gmail.com>
2024-08-23 00:40:56 -0600

    first commit

---
+++ src/components/Newsitem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let {title, description, imageUrl, newsUrl} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card" style={{width: "18rem" }}>
+                    <img src={!imageUrl?"https://ichef.bbci.co.uk/images/ic/768x432/p0g6j1tq.jpg":imageUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
